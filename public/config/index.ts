e9241d28b6617bc0f250cd49f7193a5f535e2bbe
ting.zhang <ting.zhang@medalsoft.com>
2022-05-17 16:17:19 +0800

    [update]配置更新

---
+++ public/config/index.ts
@@ -0,0 +1,29 @@
+// 此文件用于配置项目中的各种参数
+export default {
+  pages: {
+    title: 'Procurement  Contract Application Form',
+    leaveApplication: {
+      subTitle: '3.1 General Goods Or Services',
+    },
+    purchaseEquipment: {
+      subTitle: '3.2.1-1 Purchase Equipment（CAPEX）- New Investment',
+    },
+    rentEquipment: {
+      subTitle: '3.2.1-2 Rental Equipment（CAPEX）- New Investment',
+    },
+    purchaseEquipmentExpansion: {
+      subTitle:
+        '3.2.2-1 Purchase Equipment（CAPEX）- Reorganization And Expansion',
+    },
+    leasedEquipment: {
+      subTitle:
+        '3.2.1-2 Leased Equipment（CAPEX）- Reconstruction And Expansion',
+    },
+    agreement: {
+      subTitle: '2.2 Decoration agreement',
+    },
+    verhicleRental: {
+      subTitle: '2.3 vehicle rental, non capex equipment, others',
+    },
+  },
+};
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,11 +3,14 @@ import './index.less';
 import logo from '@/assets/logo.png';
 
 export default function (props: any) {
-  debugger;
+  const [title, setTitle] = useState('Procurement  Contract Application Form');
+  useEffect(() => {
+    console.log(props);
+  }, []);
   return (
     <>
       <header className="form_header">
-        <div className="form_title">Procurement Contract Application Form</div>
+        <div className="form_title">{title}</div>
         <div className="form_logo">
           <img src={logo} />
         </div>
